2a53c541e0daddaad7d9f6dcf46de2746274f7c9
Zach <walkowiakzach05@gmail.com>
2023-12-22 12:59:50 -0800

    Added flex-wrap functionality to navbar links

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -20,7 +20,7 @@ function Nav() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-wrap">
             <li className="nav-item">
               <NavLink className="nav-link" to="/manufacturers">
                 Manufacturers
